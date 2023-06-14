2ea77249db441c9c989205d34da232ebcb0115f6
Matías Morales <programacionfrida@gmail.com>
2023-06-14 15:07:56 -0300

    resolve import/export

---
+++ src/handlers/productHandlers/indexHandlers.js
@@ -0,0 +1,14 @@
+const getAllProductsHandler = require('./getAllProductsHandler');
+const getProductHandler = require('./getProductHandler');
+const postProductHandler = require('./postProductHandler');
+const putProductHandler = require('./putProductHandler');
+const deleteProductHandler = require('./deleteProductHandler');
+
+
+module.exports = {
+    getAllProductsHandler,
+    getProductHandler,
+    postProductHandler,
+    putProductHandler,
+    deleteProductHandler
+}
