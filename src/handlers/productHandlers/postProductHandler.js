32fa17f089659ab358ca70ef51b65f6a9e9f17ed
Sofía Chiabudini <sofiachiabudini@gmail.com>
2023-06-14 14:20:10 -0300

    productHandler y postProductCtrl modified to match Product collection atributes

--- src/handlers/productHandlers/postProductHandler.js
+++ src/handlers/productHandlers/postProductHandler.js
@@ -2,15 +2,15 @@ const createProduct = require('../../controllers/productCtrls/postProductCtrl');
 
 const postProductHandler = async (req, res) => {
 
-    const { name, brand, stock, price, image, description } = req.body;
+    const { name, brand, stock, price, salePrice, image, description, rating } = req.body;
 
     try {
         
-        if(!name || !brand || !stock || !price || !image || !description){
+        if(!name || !brand || !stock || !price || !image){
             return res.status(400).send({ error: 'Missing data' });
         }
 
-        const newProduct = await createProduct(name, brand, stock, price, image, description);
+        const newProduct = await createProduct(name, brand, stock, price, salePrice, image, description, rating);
 
         res.status(200).json(newProduct);
 
