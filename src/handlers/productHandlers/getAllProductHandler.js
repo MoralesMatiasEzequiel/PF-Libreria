780021c20bee5b27fa6d80c95fc04c5773ad35b4
Matías Morales <programacionfrida@gmail.com>
2023-06-14 11:51:00 -0300

    structure of router and handlers

---
+++ src/handlers/productHandlers/getAllProductHandler.js
@@ -0,0 +1,11 @@
+//importar controllers.
+
+export const getAllProductsHandler = async (req, res) => {
+
+    try {
+       
+
+    } catch (error) {
+        res.status(400).send({ error: error.message});
+    }
+};
