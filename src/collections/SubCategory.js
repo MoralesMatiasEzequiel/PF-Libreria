8dbced01d520ca06dbcf7532618bab7672fc7e08
Sofía Chiabudini <sofiachiabudini@gmail.com>
2023-06-15 20:19:48 -0300

    FIXED SUBCATEGORY SCHEMA

--- src/collections/SubCategory.js
+++ src/collections/SubCategory.js
@@ -1,15 +1,20 @@
 const { Schema, model } = require('mongoose');
 
+const categoryEnum = ['Artística', 'Oficina', 'Escolar', 'Papelería'];
+
 const subcategorySchema = new Schema({
-
-    name:{
-        type: String,
-        unique: true,
-        require: true
-    },
-
-    categories: Array
-
+  name: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  categories: {
+    type: [{
+      type: String,
+      enum: categoryEnum
+    }],
+    required: true
+  }
 });
 
 module.exports = model('Subcategory', subcategorySchema);
