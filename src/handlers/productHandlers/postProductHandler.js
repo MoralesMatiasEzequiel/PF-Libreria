bf2c97c6bc6df7d5ead306a34f7fb8c226c0fa7b
Sofía Chiabudini <sofiachiabudini@gmail.com>
2023-06-15 23:15:37 -0300

    postProduct DONE

--- src/handlers/productHandlers/postProductHandler.js
+++ src/handlers/productHandlers/postProductHandler.js
@@ -10,12 +10,29 @@ const postProductHandler = async (req, res) => {
             return res.status(400).send({ error: 'Missing data' });
         }
 
+        if (
+            typeof name !== 'string' ||
+            typeof brand !== 'string' ||
+            typeof stock !== 'number' ||
+            typeof price !== 'number' ||
+            typeof salePrice !== 'number' ||
+            typeof image !== 'string' ||
+            typeof description !== 'string' ||
+            typeof rating !== 'number' ||
+            typeof active !== 'boolean' ||
+            !Array.isArray(subcategories)
+        ){
+            return res.status(400).send({ error: 'Incorrect DataType' });
+        }
+
         const newProduct = await createProduct(name, brand, stock, price, salePrice, image, description, rating, active, subcategories);
 
-        res.status(200).json(newProduct);
+        res.status(200).send(`El producto ${newProduct.name} ha sido creado.`);
 
     } catch (error) {
         
+        return res.status(500).send(error.message);
+
     }
 };
 
