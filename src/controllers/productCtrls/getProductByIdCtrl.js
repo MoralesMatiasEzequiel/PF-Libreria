3558e4c139ed9870ae05fceeb38e48a205ce9362
Sofía Chiabudini <sofiachiabudini@gmail.com>
2023-06-17 16:28:02 -0300

    only brings active products

--- src/controllers/productCtrls/getProductByIdCtrl.js
+++ src/controllers/productCtrls/getProductByIdCtrl.js
@@ -2,7 +2,7 @@ require('../../db');
 const Product = require('../../collections/Product');
 
 const getProductByIdCtrl = async (_id) => {
-    const product = await Product.findOne({_id})
+    const product = await Product.findOne({_id, active: true})
     
     return product;
 }
