5bfbdbacf31698aa4531533c1e928272475723ef
Sofía Chiabudini <sofiachiabudini@gmail.com>
2023-06-22 15:13:22 -0300

    auth reseted

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ const userRouter = require('./userRoutes');
 const productRouter = require('./productRoutes');
 const subcategory = require('./subcategoryRoutes');
 const mercadoPagoRouter = require('./mercadoPagoRoutes');
-const authRouter = require('./authRoutes');
 
 const router = Router();
 
@@ -13,6 +12,5 @@ router.use('/user', userRouter);
 router.use('/product', productRouter);
 router.use('/subcategory', subcategory);
 router.use('/mercadopago', mercadoPagoRouter);
-router.use('/auth', authRouter);
 
 module.exports = router;
