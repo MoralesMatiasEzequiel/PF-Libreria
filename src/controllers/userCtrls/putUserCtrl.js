eb295df77dc7112d202e5ca6a06b79751e5d62a3
Sofía Chiabudini <sofiachiabudini@gmail.com>
2023-06-27 17:57:28 -0300

    Fixed user routes to match current User model

--- src/controllers/userCtrls/putUserCtrl.js
+++ src/controllers/userCtrls/putUserCtrl.js
@@ -1,11 +1,31 @@
 require('../../db');
 const User = require('../../collections/User');
 
-const putUserCtrl = async (_id, username, email, password, active) => {
-
-    const updated = await User.updateOne({_id}, {$set: {username, email, password, active}});
-
+const putUserCtrl = async (_id, name, nickname, surname, picture, emailVerified, active, reviews, favorites, orders) => {
+    const update = {
+      name,
+      nickname,
+      surname,
+      picture,
+      emailVerified,
+      active,
+    };
+  
+    if (Array.isArray(reviews)) {
+      update.$addToSet = { reviews: { $each: reviews } };
+    }
+  
+    if (Array.isArray(orders)) {
+      update.$addToSet = { orders: { $each: orders } };
+    }
+  
+    if (Array.isArray(favorites)) {
+      update.favorites = favorites;
+    }
+  
+    const updated = await User.findOneAndUpdate({ _id }, update, { new: true });
+  
     return updated;
-};
-
-module.exports = putUserCtrl;
+  };
+  
+  module.exports = putUserCtrl;
