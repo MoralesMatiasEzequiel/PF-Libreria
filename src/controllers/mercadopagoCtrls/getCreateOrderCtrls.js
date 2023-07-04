ec8a3956cdcd1fb10663c1fd04e9459bbe9883ad
Matías Morales <programacionfrida@gmail.com>
2023-07-04 20:49:52 -0300

    supermer-k2

--- src/controllers/mercadopagoCtrls/getCreateOrderCtrls.js
+++ src/controllers/mercadopagoCtrls/getCreateOrderCtrls.js
@@ -4,16 +4,16 @@ const getCreateOrderCtrls = async (data) => {
     mercadopago.configure({
         access_token: "TEST-7273759822704315-062219-6d2a1fb0b437e337c97d2cc1c77bae5e-1405802464"
     });
-    // console.log(data[0].price);
+    console.log(data);
     const items = data.map((item) => {
       return {
-        title: item.name,
-        unit_price: item.price,
+        title: item.title,
+        unit_price: item.unit_price,
         currency_id: "ARS",
         quantity: 1,
-      };
-
+      };  
     });
+    // console.log(items);
 
     const result = await mercadopago.preferences.create({ //Aquí creamos una orden de compra
         items: items,
@@ -23,60 +23,7 @@ const getCreateOrderCtrls = async (data) => {
             pending: "http://localhost:3000/mercadopago/pending",
         },
     })
-    // console.log(result.init_point);
     return result;
 };
 
 module.exports = getCreateOrderCtrls;
-
-/*
-curl -X POST \
-      'https://api.mercadopago.com/v1/payments' \
-      -H 'Authorization: Bearer YOUR_ACCESS_TOKEN' \
-      -H 'Content-Type: application/json' \ 
-      -d '{
-  "additional_info": {
-    "items": [
-      {
-        "id": "MLB2907679857",
-        "title": "Point Mini",
-        "description": "Producto Point para cobros con tarjetas mediante bluetooth",
-        "picture_url": "https://http2.mlstatic.com/resources/frontend/statics/growth-sellers-landings/device-mlb-point-i_medium@2x.png",
-        "category_id": "electronics",
-        "quantity": 1,
-        "unit_price": 58.8
-      }
-    ],
-    "payer": {
-      "first_name": "Test",
-      "last_name": "Test",
-      "phone": {
-        "area_code": 11,
-        "number": "987654321"
-      },
-      "address": {}
-    },
-    "shipments": {
-      "receiver_address": {
-        "zip_code": "12312-123",
-        "state_name": "Rio de Janeiro",
-        "city_name": "Buzios",
-        "street_name": "Av das Nacoes Unidas",
-        "street_number": 3003
-      }
-    }
-  },
-  "description": "Payment for product",
-  "external_reference": "MP0001",
-  "installments": 1,
-  "metadata": {},
-  "payer": {
-    "entity_type": "individual",
-    "type": "customer",
-    "identification": {}
-  },
-  "payment_method_id": "visa",
-  "token": "ff8080814c11e237014c1ff593b57b4d",
-  "transaction_amount": 58.8
-}
-*/
