d05d5708111521272ab0ebbec4bb75880c6e0a86
Matías Morales <programacionfrida@gmail.com>
2023-07-05 12:18:43 -0300

    Mercadopago config OK

--- src/controllers/mercadopagoCtrls/getCreateOrderCtrls.js
+++ src/controllers/mercadopagoCtrls/getCreateOrderCtrls.js
@@ -2,27 +2,27 @@ const mercadopago = require('mercadopago');
 
 const getCreateOrderCtrls = async (data) => {
     mercadopago.configure({
-        access_token: "TEST-7273759822704315-062219-6d2a1fb0b437e337c97d2cc1c77bae5e-1405802464"
+        access_token: "TEST-1788868810849886-062111-038d8e78e6211c575f9dc76a1a743ed9-701537376"
     });
-    console.log(data);
+    // console.log(data);
     const items = data.map((item) => {
       return {
-        title: item.title,
-        unit_price: item.unit_price,
+        title: item.name,
+        unit_price: item.price,
         currency_id: "ARS",
         quantity: 1,
       };  
     });
-    // console.log(items);
-
+    
     const result = await mercadopago.preferences.create({ //Aquí creamos una orden de compra
-        items: items,
-        back_urls: { //Aquí especificamos hacía dónde queremos enviar una respuesta.
-            success: "http://localhost:3000/mercadopago/success",
-            failure: "http://localhost:3000/mercadopago/failure",
-            pending: "http://localhost:3000/mercadopago/pending",
-        },
+      items: items,
+      back_urls: { //Aquí especificamos hacía dónde queremos enviar una respuesta.
+        success: "http://localhost:3000/success",
+        failure: "http://localhost:3000/mercadopago/failure",
+        pending: "http://localhost:3000/mercadopago/pending",
+      },
     })
+    // console.log(result);
     return result;
 };
 
--- src/handlers/mercadopagoHandlers/getCreateOrderHandler.js
+++ src/handlers/mercadopagoHandlers/getCreateOrderHandler.js
@@ -11,7 +11,7 @@ const getCreateOrderHandler = async (req, res) => {
 
         const createOrder = await getCreateOrderCtrls(data);
         const paymentLink = createOrder.body.init_point;
-
+        
         res.status(200).send(paymentLink);
 
     } catch (error) {
